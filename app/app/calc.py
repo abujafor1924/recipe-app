0e6ba7db540803aee9b417a3b739e4d71f435f6c
abu-jafor <abujaforwd@yahoo.com>
2025-12-26 20:14:39 +0600

    add error intended

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,16 +1,14 @@
 """
-Calculator Fuctions for Counting Operations
-Docstring for app.app.calc
+Calculator functions for counting operations.
+Docstring for app.calc
 """
 
+
 def add(one, two):
-     """
-     Add one and two and return the result.
-     """
-     return one + two
-# end def
+    """Add one and two and return the result."""
+    return one + two
+
+
 def sub(one, two):
-     """
-     Subtract two from one and return the result.
-     """
-     return one - two
+    """Subtract two from one and return the result."""
+    return one - two
