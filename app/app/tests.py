8c9b873b32e0625fc4c96492153ac97702f1fcfc
abu-jafor <abujaforwd@yahoo.com>
2025-12-26 20:09:44 +0600

    add error intended

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,13 +1,14 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class SimpleTest(SimpleTestCase):
-     def test_add_numbers(self):
-          """Test adding two numbers together."""
-          res = calc.add(5, 6)
-          self.assertEqual(res, 11)
-# end class
-     def test_subtract_numbers(self):
-          """Test subtracting two numbers."""
-          res = calc.sub(10, 14)
-          self.assertEqual(res, -4)
+    def test_add_numbers(self):
+        """Test adding two numbers together."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting two numbers."""
+        res = calc.sub(10, 14)
+        self.assertEqual(res, -4)
