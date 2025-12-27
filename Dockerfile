14823eb8578acabdf08917903dede0160db6ba57
abu-jafor <abujaforwd@yahoo.com>
2025-12-27 22:18:47 +0600

    add database test case

--- Dockerfile
+++ Dockerfile
@@ -22,9 +22,19 @@ ARG DEV=false
 # Install Python dependencies
 RUN python -m venv /py && \
     /py/bin/pip install --upgrade pip setuptools wheel && \
+    apk add --update --no-cache postgresql-client && \
+    apk add --update --no-cache --virtual .tmp-build-deps \
+        gcc \
+        musl-dev \
+        postgresql-dev \
+        libffi-dev \
+        openssl-dev \
+        cargo && \
+        build-base && \
     /py/bin/pip install -r /tmp/requirements.txt && \
     if [ "$DEV" = "true" ] ; then /py/bin/pip install -r /tmp/requirements.dev.txt ; fi && \
     rm -rf /tmp && \
+    apk del .tmp-build-deps && \
     adduser \
         --disabled-password \
         --no-create-home \
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+import time
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2Error
+
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+
+    def check_db(self, database):
+        connections[database].cursor()
+
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_up = False
+
+        while not db_up:
+            try:
+                self.check_db(database='default')
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
