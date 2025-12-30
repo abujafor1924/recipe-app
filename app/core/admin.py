d523c8aa4c3abca4ee3ff8bdba5f3e433da56c10
abujafor1924 <abujaforwd@yahoo.com>
2025-12-30 22:43:08 +0600

    add admin test case

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin for custom User model."""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    readonly_fields=['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+    search_fields = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
